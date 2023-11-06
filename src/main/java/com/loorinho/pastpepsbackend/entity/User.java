a10489fd260cb7e4d8a80b1494a99925316a319f
Loorinho <romeojacobson@gmail.com>
2023-11-06 16:37:15 +0300

    added user model

---
+++ src/main/java/com/loorinho/pastpepsbackend/entity/User.java
@@ -0,0 +1,29 @@
+package com.loorinho.pastpepsbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    private Long id;
+    private String name;
+    private String email;
+    private String registrationNumber;
+
+}
