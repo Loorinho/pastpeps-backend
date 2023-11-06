9c6bfde5e3ceb28c056338f50fdc4077eb7fbb1c
Loorinho <romeojacobson@gmail.com>
2023-11-06 16:11:24 +0300

    initial commit

---
+++ src/main/java/com/loorinho/pastpepsbackend/PastpepsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.loorinho.pastpepsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastpepsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastpepsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loorinho/pastpepsbackend/PastpepsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loorinho.pastpepsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastpepsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
